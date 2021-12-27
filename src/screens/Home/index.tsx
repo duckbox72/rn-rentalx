dba9ef817b752ffb56759357ce22a23f94a54d57
Luis Felipe Klaus <duckbox@hotmail.com>
2021-12-27 18:37:28 -0300

    Home basic using personal snippets

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import {
   Container,
+  Title,
 } from './styles';
 
 export function Home() {
   return (
     <Container>
-
+      <Title>
+        HOME
+      </Title>
     </Container>
   );
 }
