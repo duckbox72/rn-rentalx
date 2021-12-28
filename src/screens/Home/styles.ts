cd3a39f71beba6ece40c69ebf8862a37d1e07579
Luis Felipe Klaus <duckbox@hotmail.com>
2021-12-28 17:54:21 -0300

    Home/Header

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
-import theme from '../../styles/theme';
+
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
@@ -13,4 +14,20 @@ export const Header = styled.View`
   height: 113px;
 
   background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+
+  color: ${({ theme }) => theme.colors.text};
 `;
