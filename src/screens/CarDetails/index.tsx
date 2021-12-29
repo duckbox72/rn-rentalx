8e09b6fe4d05f197ee564b6e19137d2ac0505bb4
Luis Felipe Klaus <duckbox@hotmail.com>
2021-12-29 14:35:03 -0300

    ImageSlider, updates to CarDetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { StatusBar } from 'react-native'; 
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
+  CarImages,
   Header
 } from './styles';
 
 export function CarDetails() {
   return (
     <Container>
-      {/* <StatusBar 
-        barStyle='dark-content' // ANDROID
-        backgroundColor='transparent'
-        //translucent
-      /> */}
+      
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+      <CarImages>
+        <ImageSlider 
+            imageUrls={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'] }
+        />
+      </CarImages>
     </Container>
   );
 }
