d706157d053869bd69e604c6a54f6bb7276ab954
Luis Felipe Klaus <duckbox@hotmail.com>
2022-01-19 15:32:56 -0300

    MyCarsButton as floating button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-
-import { api } from '../../services/api';
+import { useNavigation} from '@react-navigation/native';
 
 import { FlatList, StatusBar } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
+import { api } from '../../services/api';
 import { Car } from '../../components/Car';
 
 import {
@@ -16,19 +16,26 @@ import {
   Header,
   HeaderContent,
   TotalCars,
+  MyCarsButton,
 } from './styles';
 import { CarDTO } from '../../dtos/CarDTO';
 import { Load } from '../../components/Load';
+import { useTheme } from 'styled-components';
 
 
 export function Home() {
   const navigation = useNavigation<any>();
+  const theme = useTheme();
 
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const handleCarDetails = (car: CarDTO) => {
     navigation.navigate('CarDetails', { car })
+  }
+
+  const handleMyCars = () => {
+    navigation.navigate('MyCars');
   }
 
   useEffect(() => {
@@ -86,6 +93,14 @@ export function Home() {
           showsVerticalScrollIndicator={false}
         />
       }
+
+      <MyCarsButton onPress={handleMyCars}>
+        <Ionicons 
+          name="ios-car-sport" 
+          size={32}
+          color={theme.colors.shape}
+        />
+      </MyCarsButton>
     </Container>
   );
 }
